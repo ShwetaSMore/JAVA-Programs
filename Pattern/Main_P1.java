736d80471ea4c55abf2d7ca14a5f0c20551d2308
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-18 15:38:12 +0530

    Add files via upload

---
+++ Pattern/Main_P1.java
@@ -0,0 +1,12 @@
+package com.Pattern;
+
+public class Main_P1 
+{
+	public static void main(String[] args) 
+	{
+		Pattern1 obj = new Pattern1();
+		obj.Patt();
+		
+	}
+
+}
---
+++ Pattern/Pattern1.java
@@ -0,0 +1,20 @@
+package com.Pattern;
+
+public class Pattern1 
+{
+	public void Patt()
+	{
+		int row = 5;
+		int col = 5;
+		
+		for(int i=1;i<=row;i++)
+		{
+			for(int j =1;j<=col;j++)
+			{
+				System.out.print("*  ");
+			}
+			System.out.println();
+		}
+	}
+
+}
