91ae185d856a2d7a192f334673a40e73a49d73d6
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-19 17:57:57 +0530

    Add files via upload

---
+++ Control Statements/Age.java
@@ -0,0 +1,19 @@
+package com.ControlStatements;
+
+public class Age 
+{
+	public static void main(String[] args) 
+	{
+		int age = 8;
+		
+		if(age>=18)
+		{
+			System.out.println("Eligible for voting");
+		}
+		else
+		{
+			System.out.println("Not eligible for voting");
+		}
+	}
+
+}
