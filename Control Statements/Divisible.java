6d880a10e99d1bd01589059d81cb43f37e4f93da
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-19 17:58:48 +0530

    Add files via upload

---
+++ Control Statements/Divisible.java
@@ -0,0 +1,20 @@
+package com.ControlStatements;
+
+public class Divisible 
+{
+	public static void main(String[] args) 
+	{
+		int num = 45;
+		
+		if(num%7==0)
+		{
+			System.out.println(num + " is divisible by 7");
+		}
+		else
+		{
+			System.out.println(num + " is not divisible by 7");
+		}
+		
+	}
+
+}
