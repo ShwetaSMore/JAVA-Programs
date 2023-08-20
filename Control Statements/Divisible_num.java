e134bcdd163266cc232ac0b197001a3ef6dbfb20
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-20 21:55:04 +0530

    Add files via upload

---
+++ Control Statements/Divisible_num.java
@@ -0,0 +1,20 @@
+package com.ControlStatements;
+
+public class Divisible_num 
+{
+	public static void main(String[] args) 
+	{
+		int num = 15;
+		
+		if(num%3==0 && num%5==0)
+		{
+			System.out.println(num +" is divisible by 3 and 5");
+		}
+		else
+		{
+			System.out.println(num + " is not divisible by 3 and 5");
+		}
+		
+	}
+
+}
