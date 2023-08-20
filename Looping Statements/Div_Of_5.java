bc1dbd62490f2243f915b7d4f7d1a746f1fa1cbd
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-20 21:53:56 +0530

    Add files via upload

---
+++ Looping Statements/Div_Of_5.java
@@ -0,0 +1,17 @@
+package com.LoopingStatements;
+
+public class Div_Of_5 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=50;i++)
+		{
+			if(i%5==0)
+			{
+				System.out.println(i);
+			}
+		}
+		
+	}
+
+}
---
+++ Looping Statements/Even_Odd_Num.java
@@ -0,0 +1,24 @@
+package com.LoopingStatements;
+
+public class Even_Odd_Num 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Even   Odd");
+		for(int i=0;i<=10;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.print(i);
+			}
+			
+			else
+			{
+				System.out.println("       "+i);
+				
+			}
+		}
+		
+	}
+
+}
