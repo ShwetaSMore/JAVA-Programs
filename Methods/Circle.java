e90a107d45a960e186f3db32efb55338bb3b3ca6
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-20 21:53:24 +0530

    Add files via upload

--- Methods/Circle.java
+++ Methods/Circle.java
@@ -1,1 +1,17 @@
+package com.Methods;
 
+public class Circle 
+{
+	public static double Area(float radius)
+	{
+		double area = 3.14*radius*radius;
+		return area;
+	}
+	
+	public static double Perimeter(float radius)
+	{
+		double perimeter = 2*3.14*radius ;
+		return perimeter;
+	}
+
+}
---
+++ Methods/Main_Circle.java
@@ -0,0 +1,15 @@
+package com.Methods;
+
+public class Main_Circle 
+{
+	public static void main(String[] args) 
+	{
+		double area = Circle.Area(3.4f);
+		System.out.println("Area of Circle is : "+area);
+		
+		double cir = Circle.Perimeter(3.4f);
+		System.out.println("Circumference of circle is : "+cir);
+		
+	}
+
+}
