708493fb171721e5592ea73c24b73313d04ace05
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-22 21:16:37 +0530

    Add files via upload

---
+++ Control Statements/EvenNo.java
@@ -0,0 +1,21 @@
+package com.ControlStatements;
+
+public class EvenNo 
+{
+	public static void main(String[] args) 
+	{
+		int no = 7;
+		
+		if(no%2==0)
+		{
+			System.out.println(no +" is a even number.");
+		}
+		else
+		{
+			System.out.println(no+" is not a even number.");
+		}
+			
+		
+	}
+
+}
