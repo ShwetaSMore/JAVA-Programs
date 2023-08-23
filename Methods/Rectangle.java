bcc1fd099459a9d1d1e72058091f6d39e02127fb
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-23 21:31:52 +0530

    Add files via upload

---
+++ Methods/Rectangle.java
@@ -0,0 +1,16 @@
+package com.Methods;
+
+public class Rectangle 
+{
+	public static float Area(float length,float breadth)
+	{
+		float area = length*breadth;
+		return area;
+	}
+	public static float Perimeter(float length,float breadth)
+	{
+		float perimeter = 2*(length+breadth);
+		return perimeter;
+	}
+
+}
