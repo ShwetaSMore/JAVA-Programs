4e52f82ff4bc34aa13491c1aee11059bf375a7eb
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-23 21:30:14 +0530

    Add files via upload

---
+++ Looping Statements/Main_Palindrome.java
@@ -0,0 +1,13 @@
+package com.LoopingStatements;
+
+public class Main_Palindrome 
+{
+	public static void main(String[] args) 
+	{
+		Palindrome obj = new Palindrome();
+		obj.PallindromeN();
+		
+	}
+
+
+}
