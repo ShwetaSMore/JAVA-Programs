2c649cd9d201e04e5d75a1335b33c3d2394a67f4
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-23 21:31:26 +0530

    Add files via upload

---
+++ Methods/Main_Rectangle.java
@@ -0,0 +1,16 @@
+package com.Methods;
+
+public class Main_Rectangle 
+{
+	public static void main(String[] args) 
+	{
+		float area = Rectangle.Area(2.2f,3.5f);
+		System.out.println("Area of Rectangle is : "+area);
+		
+		float perimeter = Rectangle.Perimeter(2.2f, 3.5f);
+		System.out.println("Perimeter of Rectangle is : "+perimeter);
+				
+		
+	}
+
+}
