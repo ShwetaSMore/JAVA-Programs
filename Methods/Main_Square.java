aaf2d7acc06b1219445a4fcc4ff143121380a6d9
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-24 11:25:08 +0530

    Add files via upload

---
+++ Methods/Main_Square.java
@@ -0,0 +1,15 @@
+package com.Methods;
+
+public class Main_Square 
+{
+	public static void main(String args[])
+	{
+		float area = Square.Area();
+		System.out.println("Area of Square is : "+area);
+		
+		float perimeter = Square.Perimeter();
+		System.out.println("Perimeter of Square is : "+perimeter);
+		
+	}
+
+}
