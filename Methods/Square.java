b6557aee53a370b6563a8086291a1870f0e0d45c
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-24 11:25:39 +0530

    Add files via upload

---
+++ Methods/Square.java
@@ -0,0 +1,19 @@
+package com.Methods;
+
+public class Square 
+{
+	static int side=10;
+	
+	public static float Area()
+	{
+		float area = side*side;
+		return area;
+	}
+	
+	public static float Perimeter()
+	{
+		float perimeter = 4*side;
+		return perimeter;
+	}
+
+}
