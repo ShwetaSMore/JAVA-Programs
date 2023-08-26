da3cfad4dab62116697230679f158dcb8dc5cffc
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-26 13:11:24 +0530

    Add files via upload

---
+++ Operator/Book.java
@@ -0,0 +1,16 @@
+package com.operator;
+
+public class Book {
+	
+	public static void main(String[] args)
+	{
+		int qnty = 3;
+		float price = 35f;
+		
+		float total = qnty*price;
+		
+		System.out.println("Total price of books : "+ total +" rs");
+				
+	}
+
+}
