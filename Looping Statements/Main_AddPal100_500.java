a9dcfccb861db923747065d0192f519a0aad8c04
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-27 19:15:01 +0530

    Add files via upload

---
+++ Looping Statements/Main_AddPal100_500.java
@@ -0,0 +1,13 @@
+package com.LoopingStatements;
+
+public class Main_AddPal100_500 
+{
+	public static void main(String[] args) 
+	{
+		AddPal100_500 obj = new AddPal100_500();
+		
+		obj.palindrome();
+		
+	}
+
+}
