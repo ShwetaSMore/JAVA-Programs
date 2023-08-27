5c36a6f05a13f6017a2ed5ad2d1ca2224ea38e21
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-27 19:14:16 +0530

    Add files via upload

---
+++ Control Statements/Number.java
@@ -0,0 +1,26 @@
+package com.ControlStatements;
+
+public class Number 
+{
+	public static void main(String[] args) 
+	{
+		int num = 0;
+		
+		if(num>0)
+		{
+			System.out.println(num + " is a Positive number.");
+		}
+		else if(num<0)
+		{
+			System.out.println(num +" is a negative number.");
+		}
+		else
+		{
+			System.out.println(num +" is equal to zero.");
+		}
+		
+			
+		
+	}
+
+}
