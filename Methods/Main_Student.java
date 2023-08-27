fd7d76b423dc46a496c27eb0a5462362da1fb0f5
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-27 19:17:40 +0530

    Add files via upload

---
+++ Methods/Main_Student.java
@@ -0,0 +1,16 @@
+package com.Methods;
+
+public class Main_Student 
+{
+	public static void main(String[] args) 
+	{
+		Student s1 = new Student();
+		s1.percentage("shweta", 68,87,99,89,99);
+		Student s2 = new Student();
+		s2.percentage("Suya", 57,99, 78, 46, 44);
+		Student s3 = new Student();
+		s3.percentage("Saku", 78, 66,98, 78, 89);
+		
+	}
+
+}
