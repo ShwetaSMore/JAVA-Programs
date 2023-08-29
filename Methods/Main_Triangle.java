9f008ae13d32c0ed7daf5733792cf8639259a55b
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-29 21:16:59 +0530

    Add files via upload

---
+++ Methods/Main_Triangle.java
@@ -0,0 +1,15 @@
+package com.Methods;
+
+public class Main_Triangle 
+{
+	public static void main(String[] args) 
+	{
+		float area = Triangle.Area(4.5f, 5.1f);
+		System.out.println("Area of Triangle is : "+area);
+		
+		float perimeter = Triangle.Perimeter(2.3f, 4.1f, 5.1f);
+		System.out.println("Perimeter of Triangle is : "+perimeter);
+		
+	}
+
+}
