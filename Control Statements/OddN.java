37983d0d39bd0c41ee31a4f4fef19127dc88b42f
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-29 21:14:57 +0530

    Add files via upload

---
+++ Control Statements/OddN.java
@@ -0,0 +1,20 @@
+package com.ControlStatements;
+
+public class OddN 
+{
+	public static void main(String[] args) 
+	{
+		int num = 2;
+		
+		if(num%2==1)
+		{
+			System.out.println(num + " is a odd number.");
+		}
+		else
+		{
+			System.out.println(num + " is not a odd number.");
+		}
+		
+	}
+
+}
