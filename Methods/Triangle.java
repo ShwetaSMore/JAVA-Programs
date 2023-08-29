b8d86a7f896f11ec90e78d413cde9ea6da6b9605
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-29 21:16:35 +0530

    Add files via upload

---
+++ Methods/Triangle.java
@@ -0,0 +1,17 @@
+package com.Methods;
+
+public class Triangle 
+{
+	public static float Area(float height,float base)
+	{
+		float area = (height*base)/2;
+		return area;
+	}
+	
+	public static float Perimeter(float a,float b,float c)
+	{
+		float perimeter = a+b+c;
+		return perimeter;
+	}
+
+}
