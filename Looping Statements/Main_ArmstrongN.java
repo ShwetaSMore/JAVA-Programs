b89da1059ab9305e6eb6ec27b72468f181907395
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-29 21:15:29 +0530

    Add files via upload

---
+++ Looping Statements/Main_ArmstrongN.java
@@ -0,0 +1,13 @@
+package com.LoopingStatements;
+
+public class Main_ArmstrongN 
+{
+	public static void main(String[] args) 
+	{
+		ArmstrongN obj = new ArmstrongN();
+		
+		obj.ArmStrong();
+		
+	}
+
+}
