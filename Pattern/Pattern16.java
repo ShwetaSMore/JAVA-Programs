a05a0e279f37532b950005bce8c17e2b082fe314
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-30 20:17:59 +0530

    Add files via upload

---
+++ Pattern/Pattern16.java
@@ -0,0 +1,21 @@
+package com.Pattern;
+
+public class Pattern16 
+{
+	public void Patt()
+	{
+		int row = 5;
+		//int col = 5;
+		
+		for(int i=1;i<=row;i++)
+		{
+			for(int j = 1;j<=i;j++)
+			{
+				System.out.print("*  ");
+					
+			}
+			System.out.println();
+		}
+	}
+
+}
