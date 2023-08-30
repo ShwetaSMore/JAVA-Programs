131b5ef4cadac98645d9c76948895350114d1d0b
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-08-30 20:21:31 +0530

    Add files via upload

---
+++ Operator/RelationalOperator.java
@@ -0,0 +1,27 @@
+package com.operator;
+
+public class RelationalOperator {
+	public static void main(String[] args)
+	{
+		int a = 10;
+		int b = 20;
+		
+		boolean bool = (a<b);
+		System.out.println(bool);
+		
+		boolean bool1 = (a>b);
+		System.out.println(bool1);
+		
+		boolean bool2 = (a<=b);
+		System.out.println(bool2);
+		
+		boolean bool3 = (a>=b);
+		System.out.println(bool3);
+		
+		boolean bool4 = (a==b);
+		System.out.println(bool4);
+		
+	}
+	
+
+}
