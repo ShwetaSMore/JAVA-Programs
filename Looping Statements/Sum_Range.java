2b543c1af0d253858db283d50f90d5484b15a833
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-09-03 20:27:37 +0530

    Add files via upload

---
+++ Looping Statements/Sum_Range.java
@@ -0,0 +1,22 @@
+package com.LoopingStatements;
+
+public class Sum_Range 
+{
+	public static void main(String[] args) 
+	{
+		int sum = 0;
+		
+		for(int i=300;i<=350;i++)
+		{
+			if(i%3==0)
+			{
+				sum = sum+i;
+			}
+			
+		}
+		
+		System.out.println("Sum of numbers divisible by 3 between 300-350 is : "+sum);
+		
+	}
+
+}
