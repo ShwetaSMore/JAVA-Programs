a22c96744c58ceb92a1851d0100bf5e5558a2847
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-09-05 12:11:56 +0530

    Add files via upload

---
+++ Overloading/Main_Student.java
@@ -0,0 +1,14 @@
+package com.overloading;
+
+public class Main_Student 
+{
+	public static void main(String[] args) 
+	{
+		Student obj = new Student();
+		obj.getmarks("Saku",45, 68.7f, 95);
+		obj.getmarks("Neha", 65, 48, 95, 52.5f);
+		obj.getmarks("Ruta", 75,65.0f, 58.45f, 86, 75.5f);
+		
+	}
+
+}
