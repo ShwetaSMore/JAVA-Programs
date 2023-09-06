51b5224c72a1f15673549de7305f3faa58d6c423
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-09-06 13:14:47 +0530

    Add files via upload

---
+++ Looping Statements/Main_Prime100_200.java
@@ -0,0 +1,13 @@
+package com.LoopingStatements;
+
+public class Main_Prime100_200 
+{
+	public static void main(String[] args) 
+	{
+		Prime100_200 obj = new Prime100_200();
+		
+		obj.isPrime();
+		
+	}
+
+}
