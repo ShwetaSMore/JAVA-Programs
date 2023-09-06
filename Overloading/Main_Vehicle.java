6c322ffac6121d79c79559e8383d943c62d8a454
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-09-06 13:15:55 +0530

    Add files via upload

---
+++ Overloading/Main_Vehicle.java
@@ -0,0 +1,15 @@
+package com.overloading;
+
+public class Main_Vehicle 
+{
+	public static void main(String[] args) 
+	{
+		Vehicle obj = new Vehicle();
+		
+		obj.display("Honda_City");
+		obj.display("Innova", "MH45K8448");
+		obj.display("Verna", "MH45B4444", "Sedan");
+		
+	}
+
+}
