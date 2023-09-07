0d12578504770259bb09f400c1d1bf88d488f247
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-09-07 14:18:42 +0530

    Add files via upload

---
+++ Inheritance/animal.java
@@ -0,0 +1,14 @@
+package com.inheritance;
+
+public class animal 
+{
+	String name;
+	String species;
+	
+	public animal(String name,String species)
+	{
+		this.name = name;
+		this.species = species;
+	}
+
+}
