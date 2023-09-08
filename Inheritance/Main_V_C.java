124fb92e7b708c4d668c94827cf95631a7f8cc06
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-09-08 20:53:08 +0530

    Add files via upload

---
+++ Inheritance/Main_V_C.java
@@ -0,0 +1,13 @@
+package com.inheritance;
+
+public class Main_V_C 
+{
+	public static void main(String[] args) 
+	{
+		car c = new car("Sedan","MH45AU0777","Honda");
+		c.display();
+		
+		
+	}
+
+}
