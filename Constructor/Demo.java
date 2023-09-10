0c1ccfe6576bd373fb3e683276b5d92554979e0a
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-09-10 21:05:41 +0530

    Add files via upload

--- Constructor/Demo.java
+++ Constructor/Demo.java
@@ -1,1 +1,38 @@
+package com.Constructor;
 
+public class Demo 
+{
+	int a;
+	float b;
+	String c;
+	
+	/*public Demo()                Non Parameterized Constructor
+	{
+		a = 10;
+		b = 23.3f;
+		c = 'A';
+	}*/
+	
+	/*
+	 * Demo()                        Default constructor
+	 * {
+	 * 
+	 * }
+	 */
+	
+	
+	public Demo(int a1,float b1,String c1)       //Parameterized constructor
+	{
+		a = a1;
+		b = b1;
+		c = c1;
+	}
+	
+	public void display()
+	{
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("c = "+c);
+	}
+
+}
