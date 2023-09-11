54ff7bcfc5ea38c2c423967963727e6241d56754
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-09-11 12:37:48 +0530

    Add files via upload

--- Overriding/animal.java
+++ Overriding/animal.java
@@ -1,1 +1,20 @@
+package com.Overriding;
 
+public class animal 
+{
+	String name;
+	String species;
+	
+	public animal(String name,String species)
+	{
+		this.name = name;
+		this.species = species;
+	}
+	
+	public void display()
+	{
+		System.out.println("Name : "+name);
+		System.out.println("Species : "+species);
+	}
+
+}
