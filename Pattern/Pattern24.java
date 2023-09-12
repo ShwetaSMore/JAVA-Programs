2d6d000b6e2de31c4538b69a04072ea4ba78c353
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-09-12 14:57:12 +0530

    Add files via upload

---
+++ Pattern/Pattern24.java
@@ -0,0 +1,23 @@
+package com.Pattern;
+
+public class Pattern24 
+{
+	public void Patt()
+	{
+		int row = 5;
+		int col = 5;
+		
+		for(int i=1;i<=row;i++)
+		{
+			for(int j =1;j<=col;j++)
+			{
+				if(i==3 && j==3)
+					System.out.print("#  ");
+				else
+					System.out.print("*  ");
+			}
+			System.out.println();
+		}
+	}
+
+}
