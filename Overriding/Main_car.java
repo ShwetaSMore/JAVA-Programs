b848f88c9c08a878fc19d516ce5f883cdfa2e71c
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-09-13 20:41:30 +0530

    Add files via upload

---
+++ Overriding/Main_car.java
@@ -0,0 +1,13 @@
+package com.Overriding;
+
+public class Main_car 
+{
+	public static void main(String[] args) 
+	{
+		car obj = new car("sedan","HONDA","MH420777");
+		obj.display();
+		
+		
+	}
+
+}
