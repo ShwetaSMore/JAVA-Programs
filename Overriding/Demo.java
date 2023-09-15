72b5587c859507a1f454c7b6d329dbd788eb8faa
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-09-15 15:46:36 +0530

    Add files via upload

---
+++ Overriding/Demo.java
@@ -0,0 +1,16 @@
+package com.Overriding;
+
+public class Demo 
+{
+	int a;
+	 Demo(int a)
+	 {
+		 this.a = a;
+	 }
+	 
+	 public void display()
+	 {
+		 System.out.println("In Demo class");
+	 }
+
+}
