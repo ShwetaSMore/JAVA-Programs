6031486f5b04b20993c266c9775adcff38be8511
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-09-16 19:26:07 +0530

    Add files via upload

--- ScannerClass/Student.java
+++ ScannerClass/Student.java
@@ -1,1 +1,14 @@
+package com.ScannerClass;
 
+public class Student 
+{
+	public void percentage(String name,float eng,float mar,float hindi,float sci,float maths)
+	{
+		float total = eng+mar+hindi+sci+maths;
+		float per = total/5;
+		
+		System.out.println("Percentage of "+name+" is : "+per+"%");
+	}
+
+
+}
