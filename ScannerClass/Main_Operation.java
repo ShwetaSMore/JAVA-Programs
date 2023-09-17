0941a0a1729b1285382c14a968b6bd86ba82e3cd
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-09-17 22:21:22 +0530

    Add files via upload

---
+++ ScannerClass/Main_Operation.java
@@ -0,0 +1,17 @@
+package com.ScannerClass;
+
+
+public class Main_Operation 
+{
+	
+	public static void main(String[] args) 
+	{
+		Operation o1 = new Operation();
+		o1.SwitchCase();
+		
+		
+		
+		
+	}	
+
+}
