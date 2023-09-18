7b845fa7cbff269d07f83c6a6e528c4536f4c58f
Shweta More <93419563+ShwetaSMore@users.noreply.github.com>
2023-09-18 22:25:12 +0530

    Add files via upload

--- Abstraction/Animal.java
+++ Abstraction/Animal.java
@@ -1,1 +1,30 @@
+package com.Abstraction;
 
+public abstract class Animal 
+{
+	String name;
+	String breed;
+	String location;
+	String foodtype;
+	
+	public Animal(String name,String breed,String location,String foodtype)
+	{
+		this.name = name;
+		this.breed = breed;
+		this.location = location;
+		this.foodtype = foodtype;
+	}
+	
+	public abstract String makesound(String sound);
+	
+	public abstract String eat(String eat);
+	
+	public void display()
+	{
+		System.out.println("Name : "+name);
+		System.out.println("Breed : "+breed);
+		System.out.println("Location : "+location);
+		System.out.println("Food Type : "+foodtype);
+	}
+
+}
